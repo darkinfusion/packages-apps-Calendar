999e9659a733d8df4be303d1a22703699259ac34
darkinfusion <daddyfire366@gmail.com>
2018-11-15 12:55:39 -0700

    add

---
+++ Android.mk
@@ -0,0 +1,56 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+# Include res dir from chips
+chips_dir := ../../../frameworks/opt/chips/res
+color_picker_dir := ../../../frameworks/opt/colorpicker/res
+datetimepicker_dir := ../../../frameworks/opt/datetimepicker/res
+timezonepicker_dir := ../../../frameworks/opt/timezonepicker/res
+appcompat_dir := ../../../$(SUPPORT_LIBRARY_ROOT)/v7/appcompat/res
+res_dirs := $(chips_dir) $(color_picker_dir) $(datetimepicker_dir) $(timezonepicker_dir) $(appcompat_dir) res
+src_dirs := src
+
+LOCAL_JACK_COVERAGE_INCLUDE_FILTER := com.android.calendar.*
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under,$(src_dirs))
+
+# bundled
+#LOCAL_STATIC_JAVA_LIBRARIES += \
+#        android-common \
+#        libchips \
+#        calendar-common
+
+# unbundled
+LOCAL_STATIC_JAVA_LIBRARIES := \
+        android-common \
+        libchips \
+        colorpicker \
+        android-opt-datetimepicker \
+        android-opt-timezonepicker \
+        android-support-v4 \
+        calendar-common \
+        android-support-v7-appcompat
+
+LOCAL_SDK_VERSION := current
+
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
+
+LOCAL_PACKAGE_NAME := Calendar
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags \
+                             ../../../frameworks/opt/datetimepicker/proguard.flags
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_FLAGS += --extra-packages com.android.ex.chips
+LOCAL_AAPT_FLAGS += --extra-packages com.android.colorpicker
+LOCAL_AAPT_FLAGS += --extra-packages com.android.datetimepicker
+LOCAL_AAPT_FLAGS += --extra-packages com.android.timezonepicker
+LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
+
+include $(BUILD_PACKAGE)
+
+# Use the following include to make our test apk.
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
